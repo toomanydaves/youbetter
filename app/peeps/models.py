44d3456615baa1e757a96d011aa3e9a31088c6c6
Sean Jacke <sean.jacke@learnship.com>
2020-06-25 10:51:21 +0200

    sets up observer for the creation of users

--- app/peeps/models.py
+++ app/peeps/models.py
@@ -1,5 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.signals import post_save
 
 class User(AbstractUser):
     pass
+
+def create_user_db(sender, instance, created, **kwargs):
+    if created:
+        #TODO connect to couch, create user and db, save url to user model
+        print("create user db")
+
+post_save.connect(create_user_db, sender=User, weak=False)
