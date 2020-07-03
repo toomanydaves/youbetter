abdf4623828641daa278d970132ffde1bbce7dc5
Sean Jacke <sean.jacke@learnship.com>
2020-07-03 11:08:22 +0200

    shows logged in indication and loads tasks

--- app/youbetter/templates/youbetter/home/_tasks.js
+++ app/youbetter/templates/youbetter/home/_tasks.js
@@ -1,71 +1,73 @@
 // TODO in sync, use a view to group tasks by date
-var listEl = document.createElement('ul');
-var itemTextEl = document.createElement('input');
-var today = new Date().toISOString().slice(0, 10);
+dbReady.done(function () {
+    var listEl = document.createElement('ul');
+    var itemTextEl = document.createElement('input');
+    var today = new Date().toISOString().slice(0, 10);
 
-function addItem (item) {
-    var itemEl = document.createElement('li');
-    var labelEl = document.createElement('label');
-    var checkboxEl = document.createElement('input');
+    function addItem (item) {
+        var itemEl = document.createElement('li');
+        var labelEl = document.createElement('label');
+        var checkboxEl = document.createElement('input');
 
-    checkboxEl.type = 'checkbox';
-    checkboxEl.id = item._id;
-    checkboxEl.checked = item.completed;
-    checkboxEl.setAttribute('data-rev', item._rev);
-    checkboxEl.addEventListener('change', function () {
-        var thisEl = this;
-        var thisLabelEl = thisEl.parentElement.querySelector('label')
+        checkboxEl.type = 'checkbox';
+        checkboxEl.id = item._id;
+        checkboxEl.checked = item.completed;
+        checkboxEl.setAttribute('data-rev', item._rev);
+        checkboxEl.addEventListener('change', function () {
+            var thisEl = this;
+            var thisLabelEl = thisEl.parentElement.querySelector('label')
 
-        db.put({
-            _id: thisEl.id,
-            _rev: thisEl.getAttribute('data-rev'),
-            completed: thisEl.checked,
-            description: thisLabelEl.textContent
-        }).then(function (response) {
-            thisEl.setAttribute('data-rev', response.rev)          
-            thisLabelEl.style.textDecoration = thisEl.checked ? 'line-through' : 'none';
-        });
-    });
-
-    labelEl.setAttribute('for', item._id);
-    labelEl.style.paddingLeft = '5px';
-    labelEl.textContent = item.description;
-
-    itemEl.append(checkboxEl, labelEl);
-    listEl.append(itemEl);
-}
-
-listEl.style.listStyle = 'none';
-
-itemTextEl.style.width = '30%';
-itemTextEl.style.padding = '6px';
-itemTextEl.style.fontSize = 'small';
-itemTextEl.placeholder = 'To do...';
-
-itemTextEl.addEventListener('keydown', function (ev) {
-    var item = { description: this.value };
-
-    if (ev.key === 'Enter' && item.description !== '') {
-        item._id = 'task_' + new Date().toISOString();
-        item.completed = false;
-        
-        db.put(item).then(function (response) {
-            item._rev = response.rev;
-            addItem(item);
+            db.put({
+                _id: thisEl.id,
+                _rev: thisEl.getAttribute('data-rev'),
+                completed: thisEl.checked,
+                description: thisLabelEl.textContent
+            }).then(function (response) {
+                thisEl.setAttribute('data-rev', response.rev)          
+                thisLabelEl.style.textDecoration = thisEl.checked ? 'line-through' : 'none';
+            });
         });
 
-        this.value = '';
+        labelEl.setAttribute('for', item._id);
+        labelEl.style.paddingLeft = '5px';
+        labelEl.textContent = item.description;
+
+        itemEl.append(checkboxEl, labelEl);
+        listEl.append(itemEl);
     }
-});
 
-db.allDocs({
-    include_docs: true,
-    startkey: 'task_' + today,
-    endkey: 'task_' + today + '\uffff'
-}).then(function (response) {
-    response.rows.forEach(function (row) {
-        addItem(row.doc);
+    listEl.style.listStyle = 'none';
+
+    itemTextEl.style.width = '30%';
+    itemTextEl.style.padding = '6px';
+    itemTextEl.style.fontSize = 'small';
+    itemTextEl.placeholder = 'To do...';
+
+    itemTextEl.addEventListener('keydown', function (ev) {
+        var item = { description: this.value };
+
+        if (ev.key === 'Enter' && item.description !== '') {
+            item._id = 'task_' + new Date().toISOString();
+            item.completed = false;
+            
+            db.put(item).then(function (response) {
+                item._rev = response.rev;
+                addItem(item);
+            });
+
+            this.value = '';
+        }
     });
-});
 
-document.getElementById('page-content').append(itemTextEl, listEl);
+    db.allDocs({
+        include_docs: true,
+        startkey: 'task_' + today,
+        endkey: 'task_' + today + '\uffff'
+    }).then(function (response) {
+        response.rows.forEach(function (row) {
+            addItem(row.doc);
+        });
+    });
+
+    document.getElementById('page-content').append(itemTextEl, listEl);
+});
